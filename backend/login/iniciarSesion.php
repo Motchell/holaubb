b0626cf634a482533a941c845a744002e3d509af
Mitchell <hidalgomora.ma@gmail.com>
2022-05-26 03:10:40 -0400

    fix

--- backend/login/iniciarSesion.php
+++ backend/login/iniciarSesion.php
@@ -2,7 +2,7 @@
 $hostHeader = $_SERVER['HTTP_HOST'];
 $urlHeader = "https://$hostHeader/holaubb/";
 echo "$hostHeader";
-include_once("../conexion.php");
+require_once("../conexion.php");
 
 $rut = $_POST["rut_alu"];
 $query = "SELECT * FROM alumno WHERE rut_alu='$rut'";
