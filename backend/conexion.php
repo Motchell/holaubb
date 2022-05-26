1aefed5ce083953b346704a7de6676848681e4d9
Mitchell <hidalgomora.ma@gmail.com>
2022-05-26 04:57:58 -0400

    fix

--- backend/conexion.php
+++ backend/conexion.php
@@ -6,12 +6,9 @@ $db_pass = "G12taller1102";
 $db_name = "G12taller_bd";
 echo "eco 2 ";
 
-$conn = mysql_connect("$db_host", "$db_user", "$db_pass");
-mysql_select_db("$db_name");
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 echo "eco 3 ";
-/*$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
-
 if (mysqli_connect_errno()) {
     echo "No se pudo conectar a la base de datos.";
-}*/
+}
 ?>
