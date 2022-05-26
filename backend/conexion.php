bf20500dafe45fca7234834f1e6f27b02845fec2
Motchell <64595135+Motchell@users.noreply.github.com>
2022-05-26 01:54:56 -0400

    Update conexion.php

--- backend/conexion.php
+++ backend/conexion.php
@@ -1,9 +1,9 @@
 <?php
 
-$db_host = "localhost";
-$db_user = "root";
-$db_pass = "";
-$db_name = "holaubb";
+$db_host = "mysqltrans.face.ubiobio.cl";
+$db_user = "G12taller";
+$db_pass = "G12taller1102";
+$db_name = "G12taller_bd";
 
 $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
