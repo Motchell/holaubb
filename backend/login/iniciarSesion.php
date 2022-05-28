651f2f728d4271d3c98fa658637a65d0a90beda9
Mitchell <hidalgomora.ma@gmail.com>
2022-05-27 22:11:52 -0400

    fix2

--- backend/login/iniciarSesion.php
+++ backend/login/iniciarSesion.php
@@ -1,13 +1,10 @@
 <?php
 require_once("../conexion.php");
 $rut = $_POST["rut_alu"];
-echo("is parte1: '".$rut."'");
-$query = "SELECT * FROM alumno WHERE rut_alu=".$rut."";
+$query = "SELECT * FROM alumno WHERE rut_alu=$rut";
 $alumno = mysqli_query($con, $query);
 
-echo "mysqli_num_rows($alumno)";
-
-$queryTutor = "SELECT * FROM alumno INNER JOIN tutor ON alumno.id_alu=tutor.id_tutor WHERE rut_alu=".$rut."";
+$queryTutor = "SELECT * FROM alumno INNER JOIN tutor ON alumno.id_alu=tutor.id_tutor WHERE rut_alu=$rut";
 $tutor = mysqli_query($con, $queryTutor);
 
 $hostHeader = $_SERVER['HTTP_HOST'];
