22ba5efded8810854557798e97d71b141b527800
Mitchell <hidalgomora.ma@gmail.com>
2022-05-27 22:45:04 -0400

    fix

--- backend/conexion.php
+++ backend/conexion.php
@@ -1,13 +1,9 @@
 <?php
-echo "eco 1 ";
 $db_host = "mysqltrans.face.ubiobio.cl";
 $db_user = "G12taller";
 $db_pass = "G12taller1102";
 $db_name = "G12taller_bd";
-echo "eco 2 ";
-var_dump(function_exists('mysqli_connect'));
 $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
-echo "eco 3 ";
 if (mysqli_connect_errno()) {
     echo "No se pudo conectar a la base de datos.";
 }
