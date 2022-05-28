f2247284691dd8007b7c7e3f48602735071e1131
Mitchell <hidalgomora.ma@gmail.com>
2022-05-27 22:16:10 -0400

    fix2

--- backend/login/iniciarSesion.php
+++ backend/login/iniciarSesion.php
@@ -1,10 +1,10 @@
 <?php
 require_once("../conexion.php");
 $rut = $_POST["rut_alu"];
-$query = "SELECT * FROM alumno WHERE rut_alu=$rut";
+$query = "SELECT * FROM alumno WHERE rut_alu='$rut'";
 $alumno = mysqli_query($con, $query);
 
-$queryTutor = "SELECT * FROM alumno INNER JOIN tutor ON alumno.id_alu=tutor.id_tutor WHERE rut_alu=$rut";
+$queryTutor = "SELECT * FROM alumno INNER JOIN tutor ON alumno.id_alu=tutor.id_tutor WHERE rut_alu='$rut'";
 $tutor = mysqli_query($con, $queryTutor);
 
 $hostHeader = $_SERVER['HTTP_HOST'];
