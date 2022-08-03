0d9db780f1df7334fc305e005d28866080522163
Mitchell <hidalgomora.ma@gmail.com>
2022-08-03 00:39:07 -0400

    updateInicio

--- frontend/inicio.php
+++ frontend/inicio.php
@@ -19,11 +19,59 @@ session_write_close();
     <title>Inicio</title>
 </head>
 <body>
-    <?php
+<?php
         include('./header.php');
-    ?>
-    <div class="container mt-5">
+        include_once('../backend/conexion.php');
+        $id_t= $_SESSION['id'];
+        $query = "SELECT * FROM actividad INNER JOIN asignar ON actividad.id_act=asignar.id_act WHERE id_tutorado= $id_t";
+        $alumno = mysqli_query($con, $query);
+        $numact = mysqli_num_rows($alumno);
         
-    </div>
+    ?>
+
+    <?php if (!($_SESSION["esAdmin"]) && $numact>0) : ?>
+
+                  
+        <div class="container mt-5">
+            <div class="row justify-content-center">
+                <div class="col-md-9 mt-3">
+                    <div class="card">
+                        <div class="card-header">
+                            <p>Tienes <?php echo $numact?> en curso</p>
+                        </div>
+                        <div class="resp-table p-4">
+                        <table class="table table-responsive align-middle align-text-center">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Nombre</th>
+                                    <th scope="col">Descripcion</th>
+                                    <th scope="col">Fecha Inicio</th>
+                                    <th scope="col">Fecha Termino</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            <?php
+                                while($rows = mysqli_fetch_array($alumno)){
+                                    $id_act = $rows['id_act'];
+                                    $nom_act = $rows['nom_act'];
+                                    $desc_act = $rows['desc_act'];
+                                    $fecha_ini = $rows['fecha_ini'];
+                                    $fecha_fin = $rows['fecha_fin'];
+                            ?>
+                            <tr>
+                                <td><?php echo "$nom_act"?></td>
+                                <td><?php echo "$desc_act"?></td>
+                                <td><?php echo "$fecha_ini"?></td>
+                                <td><?php echo "$fecha_fin"?></td>
+                            </tr>
+                        <?php
+                            }
+                        ?> 
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    <?php endif; ?>
 </body>
 </html>
